f4426ff79c2155028c27179f3bf643fbd6060872
Rositza Dineva <rositzadineva77@gmail.com>
2026-04-05 20:16:55 -0500

    Imported necessary JavaFX components

--- src/main/java/RewardsAccount.java
+++ src/main/java/RewardsAccount.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 
 public class RewardsAccount {
     private String accountId;
