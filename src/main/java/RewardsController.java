8c1b34eb1a70ba690cfd2502f4495a45e1866ecd
Rositza Dineva <rositzadineva77@gmail.com>
2026-04-05 20:48:27 -0500

    Imported all necessary dependencies

--- src/main/java/RewardsController.java
+++ src/main/java/RewardsController.java
@@ -1,3 +1,12 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import org.kordamp.ikonli.Ikonli;
+
+import javax.swing.*;
 import java.util.List;
 
 public class RewardsController {
